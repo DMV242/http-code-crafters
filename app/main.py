0440e04f6d863923fa86813e0789b2780be7dde1
MMEDV242 <mvouladavid@gmail.com>
2024-05-11 00:03:19 +0200

    add response with body feature

--- app/main.py
+++ app/main.py
@@ -1,21 +1,29 @@
 import socket
 
-SUCCESS_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n"
-NOT_FOUND_RESPONSE = "HTTP/1.1 404 Not Found\r\n\r\n"
+CRLF = "\r\n"
+
+SUCCESS_RESPONSE = "HTTP/1.1 200 OK"
+NOT_FOUND_RESPONSE = "HTTP/1.1 404 Not Found"
 
 
 def handle_request(client_socket: socket.socket) -> None:
 
     request = client_socket.recv(1024).decode()
+
     request_arr = request.split(" ")
+    print(request_arr)
     method = request_arr[0]
     path = request_arr[1]
     http_version = request_arr[2]
 
     if method.lower() == "get" and path.lower() == "/":
-        client_socket.send(SUCCESS_RESPONSE.encode())
+        client_socket.send(f"SUCCESS_RESPONSE{CRLF}{CRLF}".encode())
+    elif method.lower() == "get" and "/echo/" in path.lower():
+        param = path.split("/")[-1]
+        response = f"SUCCESS_RESPONSE{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(param)}{CRLF}{CRLF}{param}"
+        client_socket.send(response.encode())
     else:
-        client_socket.send(NOT_FOUND_RESPONSE.encode())
+        client_socket.send(f"NOT_FOUND_RESPONSE{CRLF}{CRLF}".encode())
     client_socket.close()
 
 
