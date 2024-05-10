ca4b99098da56f73fb4f5e6adc907c3585931aed
MMEDV242 <mvouladavid@gmail.com>
2024-05-10 22:11:12 +0200

    test 200

--- app/main.py
+++ app/main.py
@@ -5,8 +5,13 @@ def main():
 
     print("Logs from your program will appear here!")
 
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
+
+    print("server is listening on 4221")
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_connection, client_address = server_socket.accept()
+    print(f"connection form {client_address}")
+    client_connection.sendall(response.encode())
 
 
 if __name__ == "__main__":
