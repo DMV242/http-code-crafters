2edb6973d913c66d497745202a29f5879bc331c7
MMEDV242 <mvouladavid@gmail.com>
2024-05-19 23:12:38 +0200

    encoding gzip feature

--- app/main.py
+++ app/main.py
@@ -12,7 +12,11 @@ SERVER_ERROR_RESPONSE = "HTTP/1.1 500 Server Error"
 
 
 def format_response(
-    body: str = "", not_found: bool = False, is_file: bool = False, new_file=False
+    body: str = "",
+    not_found: bool = False,
+    is_file: bool = False,
+    new_file=False,
+    accept_encoding: bool = False,
 ) -> bytes:
     """
     This function formats response will be send to client after handling request
@@ -24,12 +28,14 @@ def format_response(
         if not is_file:
             res = f"{SUCCESS_RESPONSE}{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(body)}{CRLF}{CRLF}{body}"
         else:
-            res = f"{SUCCESS_RESPONSE}{CRLF}Content-Type: application/octet-stream{CRLF}Content-Length: {len(body)}{CRLF}{CRLF}{body}"
+            if not accept_encoding:
+                res = f"{SUCCESS_RESPONSE}{CRLF}Content-Type: application/octet-stream{CRLF}Content-Length: {len(body)}{CRLF}{CRLF}{body}"
+            else:
+                res = f"{SUCCESS_RESPONSE}{CRLF}Content-Encoding: gzip{CRLF}Content-Type: application/octet-stream{CRLF}Content-Length: {len(body)}{CRLF}{CRLF}{body}"
     if body == "" and not not_found:
         res = f"{SUCCESS_RESPONSE}{CRLF}{CRLF}"
     if new_file == True:
         res = f"{CREATED_RESPONSE}{CRLF}{CRLF}"
-    print(res)
     return res.encode()
 
 
@@ -79,10 +85,15 @@ def handle_request(client_socket: socket.socket, args: list[str]) -> None:
         client_socket.send(format_response())
     elif method.lower() == "get" and "/echo/" in path.lower():
         param = path.split("/")[-1]
-        client_socket.send(format_response(body=param))
+        encoding = request_arr[-1].split("\r\n\r\n")[0].lower()
+        if encoding == "gzip":
+            client_socket.send(format_response(body=param, accept_encoding=True))
+        else:
+            client_socket.send(format_response(body=param))
     elif method.lower() == "get" and "/user-agent" in path.lower():
         user_agent = request_arr[-1].replace(CRLF, "")
         client_socket.send(format_response(body=user_agent))
+
     else:
         client_socket.send(format_response(not_found=True))
     client_socket.close()
