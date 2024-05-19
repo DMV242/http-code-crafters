8f6205f5d8625bd4d1d78d3a55f4581c3e3a9ce4
MMEDV242 <mvouladavid@gmail.com>
2024-05-19 23:36:06 +0200

    fix bug encoding stage

--- app/main.py
+++ app/main.py
@@ -26,7 +26,10 @@ def format_response(
         res = f"{NOT_FOUND_RESPONSE}{CRLF}{CRLF}"
     elif body != "" and not not_found:
         if not is_file:
-            res = f"{SUCCESS_RESPONSE}{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(body)}{CRLF}{CRLF}{body}"
+            if not accept_encoding:
+                res = f"{SUCCESS_RESPONSE}{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(body)}{CRLF}{CRLF}{body}"
+            else:
+                res = f"{SUCCESS_RESPONSE}{CRLF}Content-Encoding: gzip{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(body)}{CRLF}{CRLF}{body}"
         else:
             if not accept_encoding:
                 res = f"{SUCCESS_RESPONSE}{CRLF}Content-Type: application/octet-stream{CRLF}Content-Length: {len(body)}{CRLF}{CRLF}{body}"
@@ -83,25 +86,19 @@ def handle_request(client_socket: socket.socket, args: list[str]) -> None:
         except:
             client_socket.send(format_response(not_found=True))
     elif method.lower() == "get" and path.lower() == "/":
-
         client_socket.send(format_response())
     elif method.lower() == "get" and "/echo/" in path.lower():
-
         param = path.split("/")[-1]
         encoding = request_arr[-1].split("\r\n\r\n")[0].lower()
-
         if encoding == "gzip":
             client_socket.send(format_response(body=param, accept_encoding=True))
         else:
             client_socket.send(format_response(body=param))
     elif method.lower() == "get" and "/user-agent" in path.lower():
-
         user_agent = request_arr[-1].replace(CRLF, "")
         client_socket.send(format_response(body=user_agent))
     else:
-
         client_socket.send(format_response(not_found=True))
-
     client_socket.close()
 
 
