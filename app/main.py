d642807263777a816bce16a695ea32d1a36432ff
MMEDV242 <mvouladavid@gmail.com>
2024-05-20 15:19:48 +0200

    fix multiple debug

--- app/main.py
+++ app/main.py
@@ -89,12 +89,12 @@ def handle_request(client_socket: socket.socket, args: list[str]) -> None:
     elif method.lower() == "get" and "/echo/" in path.lower():
         param = path.split("/")[-1]
         encoding = request_arr
-        if "gzip," in request_arr:
+        print(encoding)
+        if "gzip," in request_arr or "gzip\r\n\r\n" in request_arr:
             if "encoding-1," in request_arr or "encoding-2\r\n\r\n" in request_arr:
-                print(1)
+
                 client_socket.send(format_response(body=param, accept_encoding=True))
             else:
-                print(1)
                 client_socket.send(format_response(body=param, accept_encoding=True))
         else:
             print(2)
